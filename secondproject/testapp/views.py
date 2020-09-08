56fb09b8cbc82b38f8e6081e7df2b4ce059a3cb9
Kesarkar <PKesarkar@ad.harman.com>
2020-09-08 17:10:44 +0530

    second commit for 2nd project

---
+++ secondproject/testapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def gm_view(request):
+    return HttpResponse('<h1> Good Morning </h1>')
+
+def ga_view(request):
+    return HttpResponse('<h1> Good Afternoon </h1>')
+
+def ge_view(request):
+    return HttpResponse('<h1> Good Evening </h1>')
